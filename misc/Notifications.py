6e0a7a745616b8020601f28a618b07b4d447fcee
Marshmello1912 <nikitastarasd@gmail.com>
2023-08-27 00:26:04 +0300

    fix state set

--- misc/Notifications.py
+++ misc/Notifications.py
@@ -7,7 +7,7 @@ config = load_config('.env')
 
 
 async def start_notification(dp: Dispatcher):
-    await Form.menu.set()
+    await dp.current_state(user=config.admin.adminId).set_state(state=Form.menu)
     await dp.bot.send_message(chat_id=config.admin.adminId,
                               text=f'{config.admin.adminName}, компьютер был запущен',
                               reply_markup=keyboard.rKeyboardMenu)
