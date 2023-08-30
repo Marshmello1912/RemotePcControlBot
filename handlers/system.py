730a3fd90dad4aeb857d5603e5d229cfcd146a50
Marshmello1912 <nikitastarasd@gmail.com>
2023-08-30 23:12:42 +0300

    Add photo func

--- handlers/system.py
+++ handlers/system.py
@@ -4,6 +4,14 @@ from keyboards import keyboard
 from states.state import Form
 
 
+async def take_photo(message: types.Message):
+    result = system.take_photo()
+    if type(result) == bytes:
+        await message.answer_photo(result)
+    else:
+        await message.answer(result)
+
+
 async def turn_off(message: types.Message):
     await message.answer("Ваш компьютер будет выключен")
     system.turn_off_system()
@@ -15,5 +23,6 @@ async def reboot(message: types.Message):
 
 
 def register_system_handlers(dp: Dispatcher):
+    dp.register_message_handler(take_photo, lambda msg:msg.text == keyboard.TakePhoto.text, state=Form.System)
     dp.register_message_handler(turn_off, lambda msg: msg.text == keyboard.TurnOff.text, state=Form.System)
     dp.register_message_handler(reboot, lambda msg: msg.text == keyboard.Reboot.text, state=Form.System)
--- remoteControl/system.py
+++ remoteControl/system.py
@@ -1,4 +1,18 @@
 import os
+from cv2 import VideoCapture, imwrite
+
+
+def take_photo():
+    cam = VideoCapture(0)
+    result, image = cam.read()
+    if result:
+        imwrite('photo.png', image)
+        with open('photo.png', 'rb') as file:
+            photo = file.read()
+        os.remove("photo.png")
+        return photo
+    else:
+        return "Камера не найдена"
 
 
 def turn_off_system():
